00894c3fe736578a609328a6693f2950f508880c
unknown <davidobrien1@outlook.com>
2019-10-28 13:02:16 +0000

    Q1 to 8 of lab 5.2 complete.  Also, created new folder for Lab 5.1

--- Week05 - AJAX and REST/Lab5.2/Server/simpleserver.py
+++ Week05 - AJAX and REST/Lab5.2/Server/simpleserver.py
@@ -1,6 +1,11 @@
 #!flask/bin/python
 from flask import Flask
 
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='../')
+            
 @app.route('/')
 def index():
     return"Hello, World!"
